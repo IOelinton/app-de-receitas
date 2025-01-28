eaa705eb8157c26ee7b9d521e8db7518c184b8c6
IOelinton <oelinton.dearaujo@gmail.com>
2025-01-28 03:28:55 -0300

    feat:estilização, e criação do modal

--- src/components/Headpage.js
+++ src/components/Headpage.js
@@ -3,19 +3,23 @@ import "../styles/Headpage.css";
 export default function Headpage() {
   return (
     <div className="Headpage">
-      <div className="headepage-left">
-        <h1>Todo Dia Uma Nova Receita</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          voluptatum, quod, quas, quia, quibusdam quidem quae, quia.
-        </p>
-        <div className="headpage-buttons-area">
-          <button>Receita Aleatoria</button>
-          <button>Lorem Ipson</button>
+      <div className="content-area">
+        <div className="headepage-left">
+          <h1>
+            Todo Dia Uma Nova <span className="important-word">Receita</span>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, quod, quas, quia, quibusdam quidem quae, quia.
+          </p>
+          <div className="headpage-buttons-area">
+            <button>Receita Aleatoria</button>
+            <button>Lorem Ipson</button>
+          </div>
         </div>
-      </div>
-      <div className="headepage-right">
-        <img src="/sanduiche.png" alt="hamburger" />
+        <div className="headepage-right">
+          <img src="/sanduiche.png" alt="hamburger" />
+        </div>
       </div>
     </div>
   );
--- src/components/RecipeCategories.js
+++ src/components/RecipeCategories.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import "../styles/RecipeCategories.css";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 export const RecipeCategories = () => {
   const [categories, setCategories] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+  const [recipe, setRecipe] = useState([]);
+
+  const [showRecipe, setShowRecipe] = useState(false);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -27,39 +29,65 @@ export const RecipeCategories = () => {
     }
   };
 
+  const getRecipes = async (e) => {
+    const BASE_URL_RECIPES = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${e.target.id}`;
+    try {
+      const response = await fetch(BASE_URL_RECIPES);
+
+      if (!response.ok) {
+        throw new Error(`Erro: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setRecipes(data.meals);
+      // console.log(data.meals);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const getRecipe = async (e) => {
+    const BASE_URL_RECIPE = `https://www.themealdb.com/api/json/v1/1/search.php?s=${e.target.id}`;
+    try {
+      const response = await fetch(BASE_URL_RECIPE);
+      if (!response.ok) {
+        throw new Error(`Erro: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(e.target.id);
+      setRecipe(data.meals);
+      console.log(data.meals);
+      setShowRecipe(true);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const UnshowRecipe = () => {
+    setShowRecipe(false);
+  };
+
   useEffect(() => {
     fetchCatergories();
   }, []);
 
-  // Configurações do carrossel
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 1024 },
+      items: 8,
+    },
+    desktop: {
+      breakpoint: { max: 1024, min: 800 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 800, min: 464 },
+      items: 3,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
   };
 
   if (loading) {
@@ -70,25 +98,75 @@ export const RecipeCategories = () => {
   }
 
   return (
-    <div className="recipe-catergories">
+    <div className="recipe-categories">
       <div className="recipe-search__container">
-        <h1>Categorias</h1>
         <div className="recipe_search__categories">
-          <Slider {...settings}>
+          <Carousel
+            responsive={responsive}
+            infinite={true}
+            autoPlay={true}
+            autoPlaySpeed={3000}
+            keyBoardControl={true}
+            customTransition="transform 500ms ease-in-out"
+            transitionDuration={500}
+            containerClass="carousel-container"
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            dotListClass="custom-dot-list-style"
+            itemClass="carousel-item-padding-40-px"
+          >
             {categories.map((category) => (
               <div
                 key={category.strCategory}
                 className="categories-card"
                 id={category.strCategory}
+                onClick={getRecipes}
               >
                 <img
                   src={category.strCategoryThumb}
                   alt={category.strCategory}
+                  id={category.strCategory}
+                  onClick={getRecipes}
                 />
-                <h3>{category.strCategory}</h3>
+                <h3 id={category.strCategory} onClick={getRecipes}>
+                  {category.strCategory}
+                </h3>
+                <div className="recipe"></div>
               </div>
             ))}
-          </Slider>
+          </Carousel>
+        </div>
+        <div className="recipes_area">
+          <div className="recipes_area__container">
+            {recipes.map((recipe) => (
+              <>
+                <div key={recipe.strMeal} className="recipes_area__card">
+                  <img
+                    src={recipe.strMealThumb}
+                    alt={recipe.strMeal}
+                    className="recipes_area__card__img"
+                    id={recipe.strMeal}
+                    onClick={getRecipe}
+                  />
+                  <h3
+                    className="recipes_area__card__title"
+                    id={recipe.strMeal}
+                    onClick={getRecipe}
+                  >
+                    {recipe.strMeal}
+                  </h3>
+                </div>
+
+                {showRecipe && recipe && (
+                  <div className="modal">
+                    <button className="close" onClick={UnshowRecipe}>
+                      X
+                    </button>
+                    <h2>Aqui tem um modal</h2>
+                  </div>
+                )}
+              </>
+            ))}
+          </div>
         </div>
       </div>
     </div>
