4807d8c22b87bd2a776315c1f2f741626b12c682
IOelinton <oelinton.dearaujo@gmail.com>
2025-02-03 05:16:31 -0300

    feat: estilizada pagina de receitas filtradas por igredientes

--- src/pages/recipesByIgredients.js
+++ src/pages/recipesByIgredients.js
@@ -2,55 +2,69 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
+import "../styles/recipesByIgredients.css";
+
 export default function RecipesByIgredients() {
   const location = useLocation();
   const Igredient = location.state?.data;
 
-  const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]); // Inicialize como array vazio
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const getRecipes = async () => {
     const BASE_URL_RECIPE = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${Igredient}`;
-    // const BASE_URL_RECIPE = `https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`;
     try {
       const response = await fetch(BASE_URL_RECIPE);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
       const data = await response.json();
-      setRecipes(data.meals);
-      console.log(data.meals);
+      setRecipes(data.meals || []); // Use um array vazio se data.meals for null
+      setLoading(false);
     } catch (err) {
-      console.log("deu ruim");
-      console.log(err);
+      setError(err.message);
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    getRecipes();
+    if (Igredient) {
+      getRecipes();
+    }
   }, [Igredient]);
 
-  console.log(recipes);
+  if (loading) {
+    return <div>loading...</div>;
+  }
 
+  if (error) {
+    return <div>Erro: {error}</div>;
+  }
   return (
-    <div>
-      <h1>Recipes By Igredients</h1>
-      {recipes.map((recipe) => (
-        <>
-          <div key={recipe.strMeal} className="recipes_area__card">
-            <img
-              src={recipe.strMealThumb}
-              alt={recipe.strMeal}
-              className="recipes_area__card__img"
-              id={recipe.strMeal}
-              // onClick={handleRedirect}
-            />
-            <h3
-              className="recipes_area__card__title"
-              id={recipe.strMeal}
-              // onClick={handleRedirect}
-            >
-              {recipe.strMeal}
-            </h3>
-          </div>
-        </>
-      ))}
+    <div className="container-recipesbyigredients">
+      <h1>Recipes By Ingredients:</h1>
+      {recipes && recipes.length > 0 ? (
+        <div className="recipes-container">
+          {recipes.map((recipe) => (
+            <div key={recipe.idMeal} className="recipes_area__card">
+              <img
+                src={recipe.strMealThumb}
+                alt={recipe.strMeal}
+                className="recipes_area__card__img"
+                id={recipe.strMeal}
+              />
+              <h3 className="recipes_area__card__title" id={recipe.strMeal}>
+                {recipe.strMeal}
+              </h3>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="container-recipesbyigredients-error">
+          <h2>No recipes found for this ingredient.</h2>
+        </div>
+      )}
     </div>
   );
 }
